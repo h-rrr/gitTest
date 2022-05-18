826334339e25ee08003d883fd182c98e11995060
‘h-rrr’ <27246646@qq.com>
2022-05-18 14:37:10 +0800

    hot-fix commit1

--- src/Hello.java
+++ src/Hello.java
@@ -2,5 +2,6 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("hello");
         System.out.println("2222");
+        System.out.println("hot-fix");
     }
 }
